99c8a5f0d9a6f09f785b2faa1d0567c25de8297b
DESKTOP-U2GNR6R\15914 <1591491800@qq.com>
2022-09-02 19:23:51 +0800

    通道数按照论文中的设定好

--- DMSN/DMSN_model.py
+++ DMSN/DMSN_model.py
@@ -26,32 +26,94 @@ class Bottleneck(nn.Module):
     extention = 4
 
     # 定义初始化的网络和参数
-    def __init__(self, inplane, midplane, stride, downsample=None, st_struc='A'):
+    def __init__(self, inplane, midplane, stride, downsample=None, st_struc='A', shortcut_type=1):
         super(Bottleneck, self).__init__()
         self.st_struc = st_struc
-        self.conv1 = nn.Conv3d(inplane, midplane, kernel_size=(1, 1, 1), stride=stride, bias=False)
-        self.bn1 = nn.BatchNorm3d(midplane)
+        self.midplane = midplane
 
-        self.conv2 = conv_T(midplane, midplane)
-        self.bn2 = nn.BatchNorm3d(midplane)
-        self.conv3 = conv_T(midplane, midplane)
-        self.bn3 = nn.BatchNorm3d(midplane)
-        self.conv4 = conv_T(midplane, midplane)
-        self.bn4 = nn.BatchNorm3d(midplane)
-        self.conv5 = conv_T(midplane, midplane)
-        self.bn5 = nn.BatchNorm3d(midplane)
+        if shortcut_type==1:
+            self.midplane = 16
+        elif shortcut_type == 2:
+            self.midplane = 32
+        elif shortcut_type == 3:
+            self.midplane =64
+        elif shortcut_type == 4:
+            self.midplane =128
 
-        self.conv6 = conv_S(midplane, midplane)
-        self.bn6 = nn.BatchNorm3d(midplane)
-        self.conv7 = conv_S(midplane, midplane)
-        self.bn7 = nn.BatchNorm3d(midplane)
-        self.conv8 = conv_S(midplane, midplane)
-        self.bn8 = nn.BatchNorm3d(midplane)
-        self.conv9 = conv_S(midplane, midplane)
-        self.bn9 = nn.BatchNorm3d(midplane)
+        print(self.midplane)
+        self.conv1 = nn.Conv3d(self.midplane * self.extention, self.midplane * self.extention, kernel_size=(1, 1, 1), stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm3d(self.midplane * self.extention)
 
-        self.conv10 = nn.Conv3d(midplane, midplane*self.extention, kernel_size=1, stride=1, bias=False)
-        self.bn10 = nn.BatchNorm3d(midplane * self.extention)
+        # stage1输入通道数为64，main stage中通道数为他的一半，分支通道数为他的四分之一
+        # 这里stage1输入了16，first_plane为32，double_plane为64
+        self.one_plane = self.midplane * 2
+        self.double_plane = self.midplane * self.extention
+
+        if self.st_struc == 'A':
+            self.conv2 = conv_T(self.double_plane, self.one_plane)
+            self.bn2 = nn.BatchNorm3d(self.one_plane)
+            self.conv7 = conv_S(self.one_plane, self.midplane)
+            self.bn7 = nn.BatchNorm3d(self.midplane)
+
+            self.conv3 = conv_T(self.one_plane, self.one_plane)
+            self.bn3 = nn.BatchNorm3d(self.one_plane)
+            self.conv8 = conv_S(self.one_plane, self.midplane)
+            self.bn8 = nn.BatchNorm3d(self.midplane)
+
+            self.conv4 = conv_T(self.one_plane, self.one_plane)
+            self.bn4 = nn.BatchNorm3d(self.one_plane)
+            self.conv6 = conv_S(self.one_plane, self.midplane)
+            self.bn6 = nn.BatchNorm3d(self.midplane)
+
+            self.conv5 = conv_T(self.one_plane, self.one_plane)
+            self.bn5 = nn.BatchNorm3d(self.one_plane)
+            self.conv9 = conv_S(self.one_plane, self.midplane)
+            self.bn9 = nn.BatchNorm3d(self.midplane)
+
+        elif self.st_struc == 'B':
+            self.conv6 = conv_S(self.double_plane, self.one_plane)
+            self.bn6 = nn.BatchNorm3d(self.one_plane)
+            self.conv2 = conv_T(self.one_plane, self.midplane)
+            self.bn2 = nn.BatchNorm3d(self.midplane)
+
+            self.conv3 = conv_T(self.one_plane, self.one_plane)
+            self.bn3 = nn.BatchNorm3d(self.one_plane)
+            self.conv7 = conv_S(self.one_plane, self.midplane)
+            self.bn7 = nn.BatchNorm3d(self.midplane)
+
+            self.conv8 = conv_S(self.one_plane, self.one_plane)
+            self.bn8 = nn.BatchNorm3d(self.one_plane)
+            self.conv4 = conv_T(self.one_plane, self.midplane)
+            self.bn4 = nn.BatchNorm3d(self.midplane)
+
+            self.conv5 = conv_T(self.one_plane, self.one_plane)
+            self.bn5 = nn.BatchNorm3d(self.one_plane)
+            self.conv9 = conv_S(self.one_plane, self.midplane)
+            self.bn9 = nn.BatchNorm3d(self.midplane)
+
+        elif self.st_struc == 'C':
+            self.conv6 = conv_S(self.double_plane, self.one_plane)
+            self.bn6 = nn.BatchNorm3d(self.one_plane)
+            self.conv2 = conv_T(self.one_plane, self.midplane)
+            self.bn2 = nn.BatchNorm3d(self.midplane)
+
+            self.conv7 = conv_S(self.one_plane, self.one_plane)
+            self.bn7 = nn.BatchNorm3d(self.one_plane)
+            self.conv3 = conv_T(self.one_plane, self.midplane)
+            self.bn3 = nn.BatchNorm3d(self.midplane)
+
+            self.conv8 = conv_S(self.one_plane, self.one_plane)
+            self.bn8 = nn.BatchNorm3d(self.one_plane)
+            self.conv4 = conv_T(self.one_plane, self.midplane)
+            self.bn4 = nn.BatchNorm3d(self.midplane)
+
+            self.conv9 = conv_S(self.one_plane, self.one_plane)
+            self.bn9 = nn.BatchNorm3d(self.one_plane)
+            self.conv5 = conv_T(self.one_plane, self.midplane)
+            self.bn5 = nn.BatchNorm3d(self.midplane)
+
+        self.conv10 = nn.Conv3d(self.midplane*self.extention, self.midplane*self.extention, kernel_size=1, stride=1, bias=False)
+        self.bn10 = nn.BatchNorm3d(self.midplane * self.extention)
         self.relu = nn.ReLU(inplace=False)
 
         self.downsample = downsample
@@ -59,12 +121,7 @@ class Bottleneck(nn.Module):
 
     def ST_A(self, T):
 
-        self.conv2 = conv_T(64, 32)
-        self.bn2 = nn.BatchNorm3d(32)
-        self.conv6 = conv_S(32, 16)
-        self.bn6 = nn.BatchNorm3d(16)
-
-        T1 = self.conv_T(64, 32)(T)
+        T1 = self.conv2(T)
         T1 = self.bn2(T1)
         T1 = self.relu(T1)
         ST1 = self.conv6(T1)
@@ -72,10 +129,6 @@ class Bottleneck(nn.Module):
         ST1 = self.relu(ST1)
         # ST1 = np.concatenate(())
 
-        self.conv3 = conv_T(32, 32)
-        self.bn3 = nn.BatchNorm3d(32)
-        self.conv7 = conv_S(32, 16)
-        self.bn7 = nn.BatchNorm3d(16)
         T2 = self.conv3(T1)
         T2 = self.bn3(T2)
         T2 = self.relu(T2)
@@ -85,10 +138,6 @@ class Bottleneck(nn.Module):
         # print(ST2.size())
         ST2 = self.relu(ST2)
 
-        self.conv4 = conv_T(32, 32)
-        self.bn4 = nn.BatchNorm3d(32)
-        self.conv8 = conv_S(32, 16)
-        self.bn8 = nn.BatchNorm3d(16)
         T3 = self.conv4(T2)
         T3 = self.bn4(T3)
         T3 = self.relu(T3)
@@ -99,10 +148,6 @@ class Bottleneck(nn.Module):
         # print(ST3.size())
         # ST3 = np.concatenate(ST2, ST3)
 
-        self.conv5 = conv_T(32, 32)
-        self.bn5 = nn.BatchNorm3d(32)
-        self.conv9 = conv_S(32, 16)
-        self.bn9 = nn.BatchNorm3d(16)
         T4 = self.conv5(T3)
         T4 = self.bn5(T4)
         T4 = self.relu(T4)
@@ -112,7 +157,10 @@ class Bottleneck(nn.Module):
         # ST4 = np.concatenate(ST3, ST4)
         # print(ST3.size())
         # print(ST4.size())
-        return np.concatenate((ST1 + ST2 + ST3 + ST4), axis=1)
+        ST4 = np.concatenate((ST1.cpu().detach(), ST2.cpu().detach(), ST3.cpu().detach(), ST4.cpu().detach()), axis=1)
+        ST4 = torch.from_numpy(ST4)
+
+        return ST4
 
     def ST_B(self, Y):
         Y1 = self.conv6(Y)
@@ -143,7 +191,9 @@ class Bottleneck(nn.Module):
         ST4 = self.bn9(ST4)
         ST4 = self.relu(ST4)
 
-        return ST1 + ST2 + ST3 + ST4
+        ST4 = np.concatenate((ST1.cpu().detach(), ST2.cpu().detach(), ST3.cpu().detach(), ST4.cpu().detach()), axis=1)
+        ST4 = torch.from_numpy(ST4)
+        return ST4
 
     def ST_C(self, S):
         S1 = self.conv6(S)
@@ -174,27 +224,28 @@ class Bottleneck(nn.Module):
         ST4 = self.bn5(ST4)
         ST4 = self.relu(ST4)
 
-        return ST1 + ST2 + ST3 + ST4
+        ST4 = np.concatenate((ST1.cpu().detach(), ST2.cpu().detach(), ST3.cpu().detach(), ST4.cpu().detach()), axis=1)
+        ST4 = torch.from_numpy(ST4)
+        return ST4
 
-    def forward(self, x):
+    def forward(self, xx):
         # 参差数据
-        residual = x
-        print(x.size())
-        out = self.relu(self.bn1(self.conv1(x)))
+        residual = xx
+
+        print(xx.size())
+        print(self.midplane * self.extention)
+        out = self.relu(self.bn1(self.conv1(xx)))
 
         if self.st_struc == 'A':
             out = self.ST_A(out)
-            print(out.size())
         elif self.st_struc == 'B':
             out = self.ST_B(out)
-            print(out.size())
         elif self.st_struc == 'C':
             out = self.ST_C(out)
-            print(out.size())
 
         out = self.relu(self.bn10(self.conv10(out)))
-        print(out.size())
-        out += residual
+        out = np.concatenate((out.detach(), residual.detach()), axis=1)
+        out = torch.from_numpy(out)
         out = self.relu(out)
 
         return out
@@ -215,16 +266,16 @@ class DMSN(nn.Module):
         self.layers = layers
 
         # stem的网络层
-        self.conv1 = nn.Conv3d(3, self.inplane, kernel_size=(7, 7, 7), stride=(1, 2, 2), padding=(3, 3, 3), bias=False)
+        self.conv = nn.Conv3d(3, self.inplane, kernel_size=(7, 7, 7), stride=(1, 2, 2), padding=(3, 3, 3), bias=False)
         self.bn1 = nn.BatchNorm3d(self.inplane)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool3d(kernel_size=(3, 3, 3), padding=1, stride=2)
 
         # 32，64，128，256是指扩大4倍之前的维度,即Identity Block的中间维度
-        self.stage1 = self.make_layer(self.block, 64, self.layers[0], shortcut_type=1, stride=1)
-        self.stage2 = self.make_layer(self.block, 128, self.layers[1], shortcut_type=2, stride=2)
-        self.stage3 = self.make_layer(self.block, 256, self.layers[2], shortcut_type=3, stride=2)
-        self.stage4 = self.make_layer(self.block, 512, self.layers[3], shortcut_type=4, stride=2)
+        self.stage1 = self.make_layer(self.block, 16, self.layers[0], shortcut_type=1, stride=1)
+        self.stage2 = self.make_layer(self.block, 32, self.layers[1], shortcut_type=2, stride=2)
+        self.stage3 = self.make_layer(self.block, 64, self.layers[2], shortcut_type=3, stride=2)
+        self.stage4 = self.make_layer(self.block, 128, self.layers[3], shortcut_type=4, stride=2)
 
         # 后续的网络
         self.avgpool = nn.AvgPool2d(7)
@@ -234,16 +285,14 @@ class DMSN(nn.Module):
 
         # stem部分:conv+bn+relu+maxpool
         print(x.size())
-        out = self.conv1(x)
+        out = self.conv(x)
         out = self.bn1(out)
         out = self.relu(out)
         # print(out.size())
         out = self.maxpool(out)
-        print(out.size())
 
         # block
         out = self.stage1(out)
-        print(out.size())
         out = self.stage2(out)
         out = self.stage3(out)
         out = self.stage4(out)
@@ -265,37 +314,37 @@ class DMSN(nn.Module):
 
         block_list = []
         if shortcut_type == 1:
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='A'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='B'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='C'))
+            block_list.append(block(self.inplane, 16, stride=1, st_struc='A', shortcut_type=shortcut_type))
+            block_list.append(block(self.inplane, 16, stride=1, st_struc='B', shortcut_type=shortcut_type))
+            block_list.append(block(self.inplane, 16, stride=1, st_struc='C', shortcut_type=shortcut_type))
         elif shortcut_type == 2:
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='A'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='B'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='C'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='A'))
+            block_list.append(block(128, 32, stride=1, st_struc='A', shortcut_type=shortcut_type))
+            block_list.append(block(128, 32, stride=1, st_struc='B', shortcut_type=shortcut_type))
+            block_list.append(block(128, 32, stride=1, st_struc='C', shortcut_type=shortcut_type))
+            block_list.append(block(128, 32, stride=1, st_struc='A', shortcut_type=shortcut_type))
         elif shortcut_type == 3:
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='A'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='B'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='C'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='A'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='B'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='C'))
+            block_list.append(block(256, 64, stride=1, st_struc='A', shortcut_type=shortcut_type))
+            block_list.append(block(256, 64, stride=1, st_struc='B', shortcut_type=shortcut_type))
+            block_list.append(block(256, 64, stride=1, st_struc='C', shortcut_type=shortcut_type))
+            block_list.append(block(256, 64, stride=1, st_struc='A', shortcut_type=shortcut_type))
+            block_list.append(block(256, 64, stride=1, st_struc='B', shortcut_type=shortcut_type))
+            block_list.append(block(256, 64, stride=1, st_struc='C', shortcut_type=shortcut_type))
         elif shortcut_type == 4:
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='A'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='B'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='C'))
-            block_list.append(block(self.inplane, midplane, stride=1, st_struc='A'))
+            block_list.append(block(512, 128, stride=1, st_struc='A', shortcut_type=shortcut_type))
+            block_list.append(block(512, 128, stride=1, st_struc='B', shortcut_type=shortcut_type))
+            block_list.append(block(512, 128, stride=1, st_struc='C', shortcut_type=shortcut_type))
+            block_list.append(block(512, 128, stride=1, st_struc='A', shortcut_type=shortcut_type))
 
         return nn.Sequential(*block_list)
 
 
 resnet = DMSN(Bottleneck, [3, 4, 6, 3])
-resnet = resnet.to(device=6)
-data = torch.autograd.Variable(
-    torch.rand(8, 3, 16, 112, 112)).to(device=6)  # if modality=='Flow', please change the 2nd dimension 3==>2
-out = resnet(data)
-print(out.size(), out)
+# resnet = resnet.to(device=6)
+# data = torch.autograd.Variable(
+#     torch.rand(8, 3, 16, 112, 112)).to(device=6)  # if modality=='Flow', please change the 2nd dimension 3==>2
+# out = resnet(data)
+# print(out.size(), out)
 # 向网络输入一个1，3，224，224的tensor
-# x = torch.randn(1, 3, 224, 224)
-# x = resnet(x)
+x = torch.randn(8, 3, 16, 112, 112)
+x = resnet(x)
 print(resnet)
